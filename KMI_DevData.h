8a29319eba4503110d66c2cb178764cc5a3a0770
Eric Bateman <eric@keithmcmillen.com>
2021-10-14 09:48:01 -0700

    -Updated PIDs and portnames for new hard coded portnames

--- KMI_DevData.h
+++ KMI_DevData.h
@@ -17,38 +17,61 @@
 
 // USB and SysEx Product IDs
 
-#define PID_AUX         0
-#define PID_STRINGPORT  1
-#define PID_SOFTSTEP    10 // or is this 1?
-#define PID_12STEP      20
-#define PID_QUNEXUS     25
-#define PID_KBOARD      26
-#define PID_APPL_CBL    27
-#define PID_QUNEO       30
-#define PID_ROGUE       31
-#define PID_KMIX        35
-#define PID_KMIX_CTL    36
-#define PID_KBP4        37
-#define PID_KBP4_BL     38
-#define PID_EM1         39
-#define PID_EM1_BL      40
+#define PID_AUX             0
+#define PID_STRINGPORT      1
+#define PID_SOFTSTEP1       10      // two ports that get translated
+#define PID_SOFTSTEP2_OLD   11      // two ports that get translated
+#define PID_SOFTSTEP        12      // hard coded portnames
+#define PID_SOFTSTEP_BL     13      // hard coded portnames
+#define PID_12STEP_OLD      20      // two ports that get translated
+#define PID_12STEP          21      // hard coded portnames
+#define PID_12STEP_BL       22      // hard coded portnames
+#define PID_QUNEXUS         25      // three ports that get translated
+#define PID_KBOARD          26      // single port
+#define PID_APPL_CBL        27
+#define PID_QUNEO           30      // single port
+#define PID_ROGUE           31      // single port
+#define PID_KMIX            35      // hard coded port names
+#define PID_KMIX_CTL        36
+#define PID_KBP4            37      // hard coded port names
+#define PID_KBP4_BL         38
+#define PID_EM1             39      // test but likely hard coded
+#define PID_EM1_BL          40
 
-#define PID_BOPPAD      117
-#define PID_BOPPAD_BL   118 // this may be USB only, confirm if sysex changes
+#define PID_BOPPAD          117
+#define PID_BOPPAD_BL       118 // this may be USB only, confirm if sysex changes
 
 // **********************************************************************
 // define all of the USB OS specific midi port names used in KMI products
 // **********************************************************************
 
-// QUNEXUS
+// QUNEXUS OLD
 
 #ifdef Q_OS_MAC
-    #define QUNEXUS_IN_P1 "QuNexus Port 1"
-    #define QUNEXUS_IN_P2 "QuNexus Port 2"
-    #define QUNEXUS_IN_P3 "QuNexus Port 3"
-    #define QUNEXUS_OUT_P1 "QuNexus Port 1"
-    #define QUNEXUS_OUT_P2 "QuNexus Port 2"
-    #define QUNEXUS_OUT_P3 "QuNexus Port 3"
+    #define QUNEXUS_OLD_IN_P1 "QuNexus Port 1"
+    #define QUNEXUS_OLD_IN_P2 "QuNexus Port 2"
+    #define QUNEXUS_OLD_IN_P3 "QuNexus Port 3"
+    #define QUNEXUS_OLD_OUT_P1 "QuNexus Port 1"
+    #define QUNEXUS_OLD_OUT_P2 "QuNexus Port 2"
+    #define QUNEXUS_OLD_OUT_P3 "QuNexus Port 3"
+#else
+    #define QUNEXUS_OLD_IN_P1 "QuNexus"
+    #define QUNEXUS_OLD_IN_P2 "MIDIIN2 (QuNexus)"
+    #define QUNEXUS_OLD_IN_P3 "MIDIIN3 (QuNexus)"
+    #define QUNEXUS_OLD_OUT_P1 "QuNexus"
+    #define QUNEXUS_OLD_OUT_P2 "MIDIOUT2 (QuNexus)"
+    #define QUNEXUS_OLD_OUT_P2 "MIDIOUT2 (QuNexus)"
+#endif
+
+// QUNEXUS NEW
+
+#ifdef Q_OS_MAC
+    #define QUNEXUS_IN_P1 "QuNexus Control Surface"
+    #define QUNEXUS_IN_P2 "QuNexus Expander"
+    #define QUNEXUS_IN_P3 "QuNexus CV"
+    #define QUNEXUS_OUT_P1 "QuNexus Control Surface"
+    #define QUNEXUS_OUT_P2 "QuNexus Expander"
+    #define QUNEXUS_OUT_P3 "QuNexus CV"
 #else
     #define QUNEXUS_IN_P1 "QuNexus"
     #define QUNEXUS_IN_P2 "MIDIIN2 (QuNexus)"
@@ -88,34 +111,61 @@
     #define QUNEO_OUT_P1 "QUNEO"
 #endif
 
-// 12 Step
+// 12 Step old
 
 #ifdef Q_OS_MAC
-    #define TWELVESTEP_IN_P1 "12Step Port 1"
-    #define TWELVESTEP_IN_P2 "12Step Port 2"
-    #define TWELVESTEP_OUT_P1 "12Step Port 1"
-    #define TWELVESTEP_OUT_P2 "12Step Port 2"
+    #define TWELVESTEP_OLD_IN_P1 "12Step Port 1"
+    #define TWELVESTEP_OLD_IN_P2 "12Step Port 2"
+    #define TWELVESTEP_OLD_OUT_P1 "12Step Port 1"
+    #define TWELVESTEP_OLD_OUT_P2 "12Step Port 2"
 #else
-    #define TWELVESTEP_IN_P1 "12Step"               // todo: test on windows
-    #define TWELVESTEP_IN_P2 "MIDIIN2 (12Step)"
+    #define TWELVESTEP_OLD_IN_P1 "12Step"               // todo: test on windows
+    #define TWELVESTEP_OLD_IN_P2 "MIDIIN2 (12Step)"
+    #define TWELVESTEP_OLD_OUT_P1 "12Step"
+    #define TWELVESTEP_OLD_OUT_P2 "MIDIOUT2 (12Step)"
+#endif
+
+// 12 Step current
+
+#ifdef Q_OS_MAC
+    #define TWELVESTEP_IN_P1 "12 Step Control Surface"
+    #define TWELVESTEP_IN_P2 "12 Step Expander"
+    #define TWELVESTEP_OUT_P1 "12 Step Control Surface"
+    #define TWELVESTEP_OUT_P2 "12 Step Expander"
+#else
+    #define TWELVESTEP_IN_P1 "12 Step"               // todo: test on windows
+    #define TWELVESTEP_IN_P2 "MIDIIN2 (12 Step)"
     #define TWELVESTEP_OUT_P1 "12Step"
-    #define TWELVESTEP_OUT_P2 "MIDIOUT2 (12Step)"
+    #define TWELVESTEP_OUT_P2 "MIDIOUT2 (12 Step)"
 #endif
 
-// SOFTSTEP
+// SOFTSTEP SSCOM
 
 #ifdef Q_OS_MAC
-    #define SS_IN_P1 "SSCOM Port 1"
-    #define SS_IN_P2 "SSCOM Port 2"
-    #define SS_OUT_P1 "SSCOM Port 1"
-    #define SS_OUT_P2 "SSCOM Port 2"
+    #define SS_OLD_IN_P1 "SSCOM Port 1"
+    #define SS_OLD_IN_P2 "SSCOM Port 2"
+    #define SS_OLD_OUT_P1 "SSCOM Port 1"
+    #define SS_OLD_OUT_P2 "SSCOM Port 2"
 #else
-    #define SS_IN_P1 "SSCOM"
-    #define SS_IN_P2 "MIDIIN2 (SSCOM)"
-    #define SS_OUT_P1 "SSCOM"
-    #define SS_OUT_P2 "MIDIOUT2 (SSCOM)"
+    #define SS_OLD_IN_P1 "SSCOM"
+    #define SS_OLD_IN_P2 "MIDIIN2 (SSCOM)"
+    #define SS_OLD_OUT_P1 "SSCOM"
+    #define SS_OLD_OUT_P2 "MIDIOUT2 (SSCOM)"
 #endif
 
+// SOFTSTEP CURRENT
+
+#ifdef Q_OS_MAC
+    #define SS_IN_P1 "SoftStep Control Surface"
+    #define SS_IN_P2 "SoftStep Expander"
+    #define SS_OUT_P1 "SoftStep Control Surface"
+    #define SS_OUT_P2 "SoftStep Expander"
+#else
+    #define SS_IN_P1 "SoftStep"
+    #define SS_IN_P2 "MIDIIN2 (SoftStep)"
+    #define SS_OUT_P1 "SoftStep"
+    #define SS_OUT_P2 "MIDIOUT2 (SoftStep)"
+#endif
 
 
 #endif // KMI_DEV_DATA_H
